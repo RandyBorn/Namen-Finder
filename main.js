b58ab4c94815580fcfe38b89fb095715a043ab9c
RandyBorn <randyborn677@yahoo.com>
2025-03-25 22:18:59 +0100

    namen

---
+++ main.js
@@ -0,0 +1,14 @@
+const classmates = [
+  "Hans Müller",
+  "Peter Schmidt",
+  "Klaus Meier",
+  "Karl-Heinz",
+  "Hans-Peter",
+  "Peter-Klaus",
+  "Klaus-Hans",
+  "Karl-Peter",
+  "Hans-Klaus",
+  "Peter-Hans",
+];
+
+const perterName = [];
